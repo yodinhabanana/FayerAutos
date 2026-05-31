56b3c3e493d54608854b9dc3528eed769f017799
Mariana Lucas <mariana.lucas@estudante.ufla.br>
2026-05-31 20:26:02 -0300

    completar posteriormente com as colunas de cada tabela, modelo a ser seguido está em "Product.java", coloquei markdown para que todos pudessem mapear de maneira correta as colunas do banco -> entidades no backend

--- backend/src/main/java/backend/model/Contact.java
+++ backend/src/main/java/backend/model/Contact.java
@@ -1,5 +1,18 @@
 package backend.model;
 
+/*## Table `contacts`
+
+### Columns
+
+| Name | Type | Constraints |
+|------|------|-------------|
+| `id` | `int4` | Primary Identity |
+| `user_id` | `int4` |  |
+| `telephone` | `varchar` |  |
+| `created_at` | `timestamp` |  |
+ */
+
+
 public class Contact {
     
 }
--- backend/src/main/java/backend/model/ProductCategory.java
+++ backend/src/main/java/backend/model/ProductCategory.java
@@ -1,4 +1,15 @@
 package backend.model;
+/*## Table `product_categories`
+
+### Columns
+
+| Name | Type | Constraints |
+|------|------|-------------|
+| `id` | `int4` | Primary Identity |
+| `created_at` | `timestamp` |  |
+| `category_name` | `varchar` |  Unique |
+| `description` | `varchar` |  Nullable |
+ */
 
 public class ProductCategory {
     
